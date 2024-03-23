1d3a03e9056314e0a6cb3f49b817f485126a41f1
Jose Tejada <jtejadavilca@gmail.com>
2024-03-23 20:47:08 +0000

    Official GoRouter implementation

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,23 +1,46 @@
 import 'package:cinemapedia/presentation/screens/screens.dart';
+import 'package:cinemapedia/presentation/views/home_views/categories_view.dart';
+import 'package:cinemapedia/presentation/views/home_views/favorites_view.dart';
+import 'package:cinemapedia/presentation/views/home_views/home_view.dart';
 
 import 'package:go_router/go_router.dart';
 
 final appRouter = GoRouter(
   initialLocation: '/',
   routes: [
-    GoRoute(
-        path: '/',
-        name: HomeScreen.name,
-        builder: (context, state) => const HomeScreen(),
-        routes: [
-          GoRoute(
-            path: 'movie/:id',
-            name: MovieScreen.name,
+    ShellRoute(
+      builder: (context, state, child) {
+        return HomeScreen(childView: child);
+      },
+      routes: [
+        GoRoute(
+            path: '/',
             builder: (context, state) {
-              final movieId = state.pathParameters['id'] ?? 'no-id';
-              return MovieScreen(movieId: movieId);
+              return const HomeView();
             },
-          ),
-        ]),
+            routes: [
+              GoRoute(
+                path: 'movie/:id',
+                name: MovieScreen.name,
+                builder: (context, state) {
+                  final movieId = state.pathParameters['id'] ?? 'no-id';
+                  return MovieScreen(movieId: movieId);
+                },
+              ),
+            ]),
+        GoRoute(
+          path: '/favorites',
+          builder: (context, state) {
+            return const FavoritesView();
+          },
+        ),
+        GoRoute(
+          path: '/categories',
+          builder: (context, state) {
+            return const CategoriesView();
+          },
+        ),
+      ],
+    ),
   ],
 );
--- lib/presentation/widgets/shared/custom_bottom_navigation_bar.dart
+++ lib/presentation/widgets/shared/custom_bottom_navigation_bar.dart
@@ -1,12 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomBottomNavigationBarWidget extends StatelessWidget {
+  static const _views = [
+    '/',
+    '/categories',
+    '/favorites',
+  ];
+
   const CustomBottomNavigationBarWidget({super.key});
+
+  int _getCurrentIndex(BuildContext context) {
+    final String location = GoRouterState.of(context).fullPath ?? '/';
+
+    for (int i = 0; i < _views.length; i++) {
+      if (location == _views[i]) return i;
+    }
+
+    return 0;
+  }
 
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
       elevation: 0,
+      currentIndex: _getCurrentIndex(context),
+      onTap: (index) => context.go(_views[index]),
       items: const [
         BottomNavigationBarItem(
           icon: Icon(Icons.home_max),
